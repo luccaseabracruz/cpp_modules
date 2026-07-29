bfc4cc0e609c39e53dd0bbb679e584fdb90e9bfe
Lucca Cruz <lucca1609.ls@gmail.com>
2026-07-29 17:15:13 +0100

    partial: PhoneBook class and tested Contact class.

---
+++ cpp_00/ex01/include/PhoneBook.hpp
@@ -0,0 +1,12 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+#include "Contacts.hpp"
+
+class	PhoneBook
+{
+	public:
+		Contact contacts[8];
+};
+
+#endif
--- cpp_00/ex01/src/main.cpp
+++ cpp_00/ex01/src/main.cpp
@@ -6,14 +6,21 @@
 /*   By: lseabra- <lseabra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/28 12:51:18 by lseabra-          #+#    #+#             */
-/*   Updated: 2026/07/28 14:40:40 by lseabra-         ###   ########.fr       */
+/*   Updated: 2026/07/29 15:49:16 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "Contact.hpp"
 
 int	main(void)
 {
-	const char * str = "lala";
-	std::cout << str << std::endl;
+	// Contact test = {
+	// 	"Fulano",
+	// 	"da Silva",
+	// 	"Fudasil",
+	// 	"+351 900000000",
+	// 	"Likes to each Bugger."
+	// };
+	// std::cout << std::join(test.first_name, test.last_name) << std::endl;
 }
