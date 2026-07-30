0dcb8434f060e7165a3575ebfec08c6d2b7e2309
Lucca Cruz <lucca1609.ls@gmail.com>
2026-07-30 11:45:28 +0100

    partial: structure for testing PhoneBook class.

--- cpp_00/ex01/src/main.cpp
+++ cpp_00/ex01/src/main.cpp
@@ -3,24 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lseabra- <lseabra-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lucca <lucca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/28 12:51:18 by lseabra-          #+#    #+#             */
-/*   Updated: 2026/07/29 15:49:16 by lseabra-         ###   ########.fr       */
+/*   Updated: 2026/07/29 19:08:57 by lucca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+static std::string	ft_input(void)
+{
+	std::string	input;
+
+	std::cout << "Choose a command (ADD | SEARCH | EXIT): ";
+	std::cin >> input;
+	return (input);
+};
+
+static ft_new_contact()
+{
+	
+}
 
 int	main(void)
 {
-	// Contact test = {
-	// 	"Fulano",
-	// 	"da Silva",
-	// 	"Fudasil",
-	// 	"+351 900000000",
-	// 	"Likes to each Bugger."
-	// };
-	// std::cout << std::join(test.first_name, test.last_name) << std::endl;
-}
+	std::string	input = ft_input();
+	PhoneBook	book;
+	int			counter = 0;
+
+	if (input == "ADD")
+		std::cout << "ADD" << std::endl;
+	else if (input == "SEARCH")
+		std::cout << "SEARCH" << std::endl;
+	else if (input == "EXIT")
+		std::cout << "EXIT" << std::endl;
+};
