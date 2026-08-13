a963de2e034eb5802480d731f7872096cc5ee39d
Lucca Cruz <lucca1609.ls@gmail.com>
2026-08-13 19:53:16 +0100

    feat: add guards in case of memory allocation failure.

--- cpp_01/ex00/src/newZombie.cpp
+++ cpp_01/ex00/src/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: lseabra- <lseabra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/13 15:39:21 by lseabra-          #+#    #+#             */
-/*   Updated: 2026/08/13 16:53:24 by lseabra-         ###   ########.fr       */
+/*   Updated: 2026/08/13 19:43:41 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,10 @@
 Zombie*	newZombie( std::string name )
 {
 	Zombie *z = new Zombie(name);
+	if (!z)
+	{
+		std::cout << "Error: Zombie allocation failed.\n";
+		return (NULL);
+	}
 	return (z);
 }
