7fe051b65a71011c550f1a3c98270175e262dbae
Lucca Cruz <lucca1609.ls@gmail.com>
2026-09-09 11:40:13 +0100

    fix: make AMateria destructor const.

--- cpp_04/ex03/include/IMateriaSource.hpp
+++ cpp_04/ex03/include/IMateriaSource.hpp
@@ -6,7 +6,7 @@
 /*   By: lucca <lucca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 14:36:37 by lucca             #+#    #+#             */
-/*   Updated: 2026/09/08 14:37:34 by lucca            ###   ########.fr       */
+/*   Updated: 2026/09/09 11:15:45 by lucca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ class IMateriaSource
 	public:
 		virtual ~IMateriaSource() {}
 		virtual void learnMateria(AMateria*) = 0;
-		virtual AMateria* createMateria(std::string const & type) = 0;
+		virtual AMateria* createMateria(std::string const & type) const = 0;
 };
 
 #endif
