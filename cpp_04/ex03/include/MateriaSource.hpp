65b7ace6159631aa51a9e99f0ebf485a92715b7d
Lucca Cruz <lucca1609.ls@gmail.com>
2026-09-09 11:41:54 +0100

    feat: implement MateriaSource.

--- cpp_04/ex03/include/MateriaSource.hpp
+++ cpp_04/ex03/include/MateriaSource.hpp
@@ -6,7 +6,7 @@
 /*   By: lucca <lucca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 14:35:08 by lucca             #+#    #+#             */
-/*   Updated: 2026/09/08 14:37:57 by lucca            ###   ########.fr       */
+/*   Updated: 2026/09/09 11:31:02 by lucca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,22 @@
 # define MATERIASOURCE_HPP
 
 # include "AMateria.hpp"
+# include "IMateriaSource.hpp"
 
-class MateriaSource
+# define KNOWN_MATERIAS_LEN 4
+
+class MateriaSource: public IMateriaSource
 {
+	private:
+		AMateria	*knownMaterias_[KNOWN_MATERIAS_LEN];
 	public:
-		~MateriaSource();
-		void learnMateria(AMateria*);
-		AMateria* createMateria(std::string const & type);
+		MateriaSource(void);
+		MateriaSource(MateriaSource const & other);
+		MateriaSource&	operator=(MateriaSource const & other);
+		~MateriaSource(void);
+
+		void learnMateria(AMateria *materia);
+		AMateria* createMateria(std::string const & type)	const;
 };
 
 #endif
--- cpp_04/ex03/src/MateriaSource.cpp
+++ cpp_04/ex03/src/MateriaSource.cpp
@@ -6,14 +6,71 @@
 /*   By: lucca <lucca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 14:38:21 by lucca             #+#    #+#             */
-/*   Updated: 2026/09/08 14:39:08 by lucca            ###   ########.fr       */
+/*   Updated: 2026/09/09 11:31:41 by lucca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MateriaSource.hpp"
 
-MateriaSource::MateriaSource(void){}
+MateriaSource::MateriaSource(void)
+{
+	for (int i = 0; i < KNOWN_MATERIAS_LEN; i++)
+		knownMaterias_[i] = NULL;
+}
 
-MateriaSource::MateriaSource(void){}
+MateriaSource::MateriaSource(MateriaSource const & other)
+{
+	*this = other;
+}
 
-MateriaSource::~MateriaSource(void){}
+MateriaSource& MateriaSource::operator=(MateriaSource const & other)
+{
+	if (this != &other)
+	{
+		for (int i = 0; i < KNOWN_MATERIAS_LEN; i++)
+		{
+			if (other.knownMaterias_[i] != NULL)
+			{
+				std::string const & type = other.knownMaterias_[i]->getType();
+				AMateria	*newMateria = other.createMateria(type);
+				this->learnMateria(newMateria);
+				delete newMateria;
+			}
+		}
+	}
+	return (*this);
+}
+
+MateriaSource::~MateriaSource(void)
+{
+	for (int i = 0; i < KNOWN_MATERIAS_LEN; i++)
+	{
+		if (knownMaterias_[i] != NULL)
+		{
+			delete knownMaterias_[i];
+			knownMaterias_[i] = NULL;
+		}
+	}
+}
+
+void	MateriaSource::learnMateria(AMateria *materia)
+{
+	for (int i = 0; i < KNOWN_MATERIAS_LEN; i++)
+	{
+		if (knownMaterias_[i] == NULL)
+		{
+			knownMaterias_[i] = materia;
+			break ;
+		}
+	}
+}
+
+AMateria	*MateriaSource::createMateria(std::string const & type) const
+{
+	for (int i = 0; i < KNOWN_MATERIAS_LEN; i++)
+	{
+		if (knownMaterias_[i] != NULL && knownMaterias_[i]->getType() == type)
+			return (knownMaterias_[i]->clone());
+	}
+	return (NULL);
+}
