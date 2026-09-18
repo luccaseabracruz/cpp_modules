f7c2aacfb22e55233114971aca3f996bb85ba0d0
Lucca Cruz <lucca1609.ls@gmail.com>
2026-09-18 14:04:16 +0100

    Fix: test member function title.

--- cpp_05/ex00/src/main.cpp
+++ cpp_05/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lucca <lucca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/15 19:24:07 by lucca             #+#    #+#             */
-/*   Updated: 2026/09/18 13:48:53 by lucca            ###   ########.fr       */
+/*   Updated: 2026/09/18 14:02:48 by lucca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main()
 {
 	const std::string	testTitles[] = {
 		"Test Grade",
-		"Test Getters",
+		"Test Member Functions",
 		"Test Special Members"
 	};
 	int (*testFunctions[])() = {
