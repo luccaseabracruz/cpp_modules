6b27e9cebff2244685614f6cf1c5dca892a73f77
Lucca Cruz <lucca1609.ls@gmail.com>
2026-09-21 12:28:42 +0100

    Revert "fix: change getter return."
    This reverts commit 0e3003e3bd3c06e276d0008adce78a6accb5a448.

--- cpp_01/ex03/include/Weapon.hpp
+++ cpp_01/ex03/include/Weapon.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Weapon.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lseabra- <lseabra-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lucca <lucca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 11:14:33 by lucca             #+#    #+#             */
-/*   Updated: 2026/09/21 12:26:21 by lseabra-         ###   ########.fr       */
+/*   Updated: 2026/08/17 12:53:01 by lucca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ class	Weapon
 	public:
 		Weapon(const std::string& newType);
 		~Weapon();
-		std::string	getType() const;
-		void		setType(const std::string& newType);
+		const std::string&	getType() const;
+		void				setType(const std::string& newType);
 };
 
 #endif
